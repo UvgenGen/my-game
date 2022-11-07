563beb9e29050b315d14713ea1e6ad13cfb6fb12
Sagirov Eugeniy <sagirov19@gmail.com>
2022-11-07 19:59:55 +0200

    feat: initial commit

---
+++ frontend-app-notifier/pages/index.js
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Test!!!123
+        </h1>
+
+        <p className={styles.description}>
+          Get started by editing
+        </p>
+      </main>
+
+      <footer className={styles.footer}>
+      </footer>
+    </div>
+  )
+}
