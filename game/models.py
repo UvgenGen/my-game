4a30f40af7ab9adb78113b3710ac074e5c6da482
Sagirov Eugeniy <sagirov19@gmail.com>
2023-06-09 22:52:46 +0300

    feat: add game model

--- game/models.py
+++ game/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Game(models.Model):
+    title = models.CharField(max_length=25)
+    password = models.CharField(max_length=15)
+    data = models.JSONField(default={})
+    max_player_count = models.IntegerField(
+        validators=[
+            MaxValueValidator(100),
+            MinValueValidator(1)
+        ]
+    )
+    players = models.ManyToManyField(User, related_name='players', blank=True)
+    creator = models.ForeignKey(User, related_name='creator', on_delete=models.CASCADE)
+    created_at = models.TimeField(auto_now_add=True)
